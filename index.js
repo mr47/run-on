6ad2062b28f339828a06c33d8b7226a7fb9b85c5
mr47 <mr4700@gmail.com>
2017-08-02 14:29:15 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 var program = require('commander');
+var platformIs = require('platform-is');
+
+function list(val) {
+  return val.split(',');
+}
 
 program
   .option('-e, --encoding [type]', 'Specify the encoding of dotenv file')
   .option('-p, --path [type]', 'Specify a custom path of dotenv file')
+  .option('-o, --on <items>', 'Specify a custom path of dotenv file', list)
   .option('-s, --silent', 'silent')
   .parse(process.argv);
 var scriptName = program.args[0];
@@ -11,6 +17,28 @@ var scriptName = program.args[0];
 if (!program.silent) {
   console.log('running better-npm-run in', process.cwd());
 }
+if (program.on && program.on.length === 1){
+  var platform = program.on[0];
+  if (platform === 'mac' && !platformIs.isMac()) process.exit();
+  if (platform === 'linux' && !platformIs.isLinux()) process.exit();
+  if (platform === 'windows' && !platformIs.isWindows()) process.exit();
+} else if (program.on && program.on.length > 1){
+  var exit = false;
+  for (var i = 0; i < program.on.length; i ++){
+    var v = program.on[i];
+    if (v === 'mac') exit = !platformIs.isMac();
+    if (v === 'linux') exit = !platformIs.isLinux();
+    if (v === 'windows') exit = !platformIs.isWindows();
+    if (!exit) break;
+  }
+  if (exit) process.exit();
+} else if (program.on && program.on.length === 0) {
+  if (program.on) {
+    process.stderr.write('ERROR: Please set run platform');
+    process.exit(1);
+  }
+}
+
 var join = require('path').join;
 var fullPackagePath = join(process.cwd(), 'package.json');
 var pkg = require(fullPackagePath);
